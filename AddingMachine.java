1349819dfe6970c92609db1d666a501292a9b324
hunterpaulson <hwp1113@gmail.com>
2020-10-05 21:17:46 -0700

    Creates AddingMachine.java and documents blank methods

---
+++ AddingMachine.java
@@ -0,0 +1,48 @@
+/* Assignment 1 for CSE360 Fall 2020
+* @author Hunter Paulson
+*/
+package cse360assignment02;
+
+
+/* Methods will be implemented on the next commit
+*/
+public class AddingMachine {
+  private int total;
+  
+  /* Constructor
+  */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /* Returns the current total
+  * @return The total
+  */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /* Adds the param to the total variable
+  * @param value the value being added to total
+  */
+  public void add (int value) {
+  }
+
+  /* subtracts param from the total variable
+  * @param value the value being subtracted from total
+  */
+  public void subtract (int value) {
+  }
+
+  /* Prints the sequence of numbers and operators
+  * @return A string of the numbers and their operators
+  */
+  public String toString () {
+    return "";
+  }
+
+  /* Clears the "memory"
+  */
+  public void clear() {
+  }
+}
